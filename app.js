1b7635e6f7afc25e3833de5a775f65adcbb048e5
Schmerb <mike.schmerbeck@gmail.com>
2017-05-08 22:10:35 -0400

    search for movie and search by id work crudely

--- app.js
+++ app.js
@@ -78,12 +78,13 @@ var state = {
 
 (function (namespace) {
     'use strict'
-    var BASE_URL = "http://api-public.guidebox.com/v2/shows/2622?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498";
+    var BASE_URL = "http://api-public.guidebox.com/v2/search?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&type=movie&field=title&query=Terminator";
     
 
     //search?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&type=show&field=title&query=Weeds
     // This searches for a show based on title, includes id for show
 
+    //shows/2622?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498
 
     //search?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&type=movie&field=title&query=Terminator
     // This searches for a movie based on its title, includes the id of that movie
@@ -133,6 +134,70 @@ var state = {
 
 
 // ================================================================================
+// Get by id
+// ================================================================================
+
+
+(function (namespace) {
+    'use strict'
+    var BASE_URL = "http://api-public.guidebox.com/v2/movies/";
+    
+
+    //search?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&type=show&field=title&query=Weeds
+    // This searches for a show based on title, includes id for show
+
+    //shows/2622?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498
+
+    //search?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&type=movie&field=title&query=Terminator
+    // This searches for a movie based on its title, includes the id of that movie
+
+    //movies/{128834}?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498"
+    // Using the id fetched above, meta data is fetched for movie including sources where it can be found
+
+    //sources?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498&filter=movie";
+
+    namespace.createRequest = function (requestData, callback, parseAsXml) {
+        // parseAsXml = !! parseAsXml;
+        // if (typeof callback !== 'function') {
+        //     throw new Error("The callback parameter was not a function");
+        // }
+        // var queryString = "type=" + (parseAsXml ? "xml" : "json");
+        // if (typeof requestData === 'string') {
+        //     queryString += "&title=" + requestData;
+        // } else if (typeof requestData === 'object' && requestData.hasOwnProperty("title")) {
+        //     queryString += "&title=" + requestData.title;
+
+        //     if (requestData.hasOwnProperty("year")) {
+        //         queryString += "&year=" + requestData.year;
+        //     }
+        // } else {
+        //     throw new Error("I don't know how to handle " + requestData);
+        // }
+
+        var queryString = requestData + "?api_key=db85b00dc1a54c2a02ed61575609802bb3d8c498";
+        var httpReq = new XMLHttpRequest();
+        httpReq.open("GET", BASE_URL + queryString.replace(/\s/ig, "%20", true)); //  + queryString.replace(/\s/ig, "%20")
+        httpReq.onreadystatechange = function () {
+            if (httpReq.readyState !== 4) {
+                return;
+            }
+
+            if (httpReq.status !== 200) {
+                console.log("Could not find title");
+                throw new Error("Unexpected HTTP Status Code (" + httpReq.status + ")");
+            } 
+
+            callback(parseAsXml ? new DOMParser()
+                .parseFromString(httpReq.responseText, "text/xml") : JSON.parse(httpReq.responseText));
+        };
+        httpReq.send();
+    };
+
+})(window.getid || (window.getid = {}));
+
+
+
+// ================================================================================
 // API calls
 // ================================================================================
 
@@ -171,8 +236,20 @@ function formSubmit() {
     });
 }
 
+
+function getById(){
+    $('.id-form').submit(function(e) {
+        e.preventDefault();
+        state.query = $(this).find('.js-id').val();
+        getid.createRequest(state.query, function (resp) {
+                console.log(resp);
+        });
+    })
+}
+
 $(function() {
     formSubmit();
+    getById();
 })
 
 
